2ab30d27c4fcf7a80139bdf83e25bd156e24b82f
Leitz92 <albertllovet01@gmail.com>
2024-08-02 12:29:05 +0200

    Completada función preparation_time_in_minutes()

--- python/02-guidos-gorgeous-lasagna/lasagna.py
+++ python/02-guidos-gorgeous-lasagna/lasagna.py
@@ -9,8 +9,9 @@ of a module and its functions and/or classes.
 
 
 # define the 'EXPECTED_BAKE_TIME' constant.✅
-# time is in minutes 
 EXPECTED_BAKE_TIME = 60
+# Time in minutes per layer of the lasagna
+PREPARATION_TIME = 2
 
 # Remove 'pass' and complete the 'bake_time_remaining()' function below. ✅
 def bake_time_remaining(elapsed_bake_time:int):
@@ -27,10 +28,19 @@ def bake_time_remaining(elapsed_bake_time:int):
     return EXPECTED_BAKE_TIME - elapsed_bake_time
 
 
-#TODO: Define the 'preparation_time_in_minutes()' function below.
+# Define the 'preparation_time_in_minutes()' function below. ✅
 # You might also consider using 'PREPARATION_TIME' here, if you have it defined.
+def preparation_time_in_minutes(number_of_layers:int):
+    """Calculate the preparation time
 
+    :param number_of_layers: int - Number of layers of the lasagna.
+    :return: int - Time in minutes you would spend adding the number of layers to the lasagna.
 
+    Function that takes the number of layers you want to add to the lasagna as an argument and returns how many minutes you would spend making them.
+    Assume each layer takes 2 minutes to prepare.
+    """
+
+    return PREPARATION_TIME * number_of_layers
 
 #TODO: define the 'elapsed_time_in_minutes()' function below.
 # Remember to add a docstring (you can copy and then alter the one from bake_time_remaining.)
