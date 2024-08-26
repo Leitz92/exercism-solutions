e2022add86a9f3b7e62bcca15e3bd6ec402d796e
Leitz92 <albertllovet01@gmail.com>
2024-08-26 18:55:23 +0200

    Added exercise reverse-string

---
+++ python/06-strings/reverse-string/reverse_string.py
@@ -0,0 +1,9 @@
+def reverse(text):
+    """
+    Reverse a given text.
+
+    :param text: str - the input text
+    :return: str - the reversed text
+    """
+    reversed_text = text[::-1]
+    return reversed_text
